d0212af76fd6964714109cdbc8a4b6931691e930
Marl <winering@gmail.com>
2024-09-27 13:21:46 -0300

    Commit 3

--- Test.cs
+++ Test.cs
@@ -34,13 +34,15 @@ internal class Test
         Console.WriteLine(club.inscribirActividad("Natacion", 0));
         Console.WriteLine("-------------------------------------------------------\n");
 
+        // Inscribimos al socio en más actividades.
         Console.WriteLine(club.inscribirActividad("Natacion", 1));
         Console.WriteLine(club.inscribirActividad("Basquet", 2));
         Console.WriteLine("El cupo actualizado para " + club.ListaActividades[2].Nombre + " es " + club.ListaActividades[2].Cupo);
         Console.WriteLine("El cupo actualizado para " + club.ListaActividades[1].Nombre + " es " + club.ListaActividades[1].Cupo);
         Console.WriteLine("-------------------------------------------------------\n");
 
-        // Excede el límite de actividades permitido.
+        // Excede el límite de actividades permitido (3 actividades max.).
+        Console.WriteLine($"Número de actividades del socio {club.ListaSocios[0].Nombre} es: {club.ListaSocios[0].Actividades.Count}");
         Console.WriteLine(club.inscribirActividad("Futbol", 1));
         Console.WriteLine("-------------------------------------------------------\n");
     }
